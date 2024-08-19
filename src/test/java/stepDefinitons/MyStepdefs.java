8735b4f0616bc61464e874aa19048a5395cc6ddd
Dilan Ozyazici <113533179+Yuukinoon@users.noreply.github.com>
2024-08-19 16:09:34 +0300

    19/08/24
    first test

--- src/test/java/stepDefinitons/MyStepdefs.java
+++ src/test/java/stepDefinitons/MyStepdefs.java
@@ -1,17 +1,42 @@
 package stepDefinitons;
 
+import io.appium.java_client.AppiumDriver;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import pages.LoginPage;
+import utilities.Driver;
+
+import java.net.MalformedURLException;
 
 public class MyStepdefs {
+    private AppiumDriver driver;
+    LoginPage loginPage;
+
     @Given("the user open the application with android {int}")
-    public void theUserOpenTheApplicationWithAndroid(int arg0) {
+    public void theUserOpenTheApplicationWithAndroid(int arg0) throws MalformedURLException, InterruptedException {
+
+            AppiumDriver driver = Driver.getDriver();
+            DesiredCapabilities androidCaps = new DesiredCapabilities();
+            androidCaps.setCapability("appium:deviceName", "sdk_gphone64_x86_64");
+            androidCaps.setCapability("appium:automationName", "UIAutomator2");
+            androidCaps.setCapability("appium:udid", "emulator-5554"); //DeviceId from "adb devices" command
+            androidCaps.setCapability("appium:platformName", "Android");
+            androidCaps.setCapability("appium:platformVersion", "14.0");
+            androidCaps.setCapability("appium:skipUnlock", "true");
+            androidCaps.setCapability("appium:app", System.getProperty("user.dir") + "\\src\\main\\resources\\isinOlsun.apk");
+            androidCaps.setCapability("appium:fullReset", "true");
+            androidCaps.setCapability("appium:noReset", "false");
+            androidCaps.setCapability("appPackage", "com.isinolsun.app");
+            System.out.println("***** Android App *****");
     }
+
 
     @When("the user clicks the {string} button")
     public void theUserClicksTheButton(String arg0) {
+
     }
 
     @And("the user clicks the search button")
--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -11,26 +11,27 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Driver {
-        public AppiumDriver driver;
-        public WebDriverWait wait;
-        public JavascriptExecutor js;
-        public String baseURL;
-        @Test
-        public void androidLaunchTest() throws MalformedURLException, InterruptedException {
-            DesiredCapabilities androidCaps = new DesiredCapabilities();
-            androidCaps.setCapability("appium:deviceName", "sdk_gphone64_x86_64");
-            androidCaps.setCapability("appium:automationName", "UIAutomator2");
-            androidCaps.setCapability("appium:udid", "emulator-5554"); //DeviceId from "adb devices" command
-            androidCaps.setCapability("appium:platformName", "Android");
-            androidCaps.setCapability("appium:platformVersion", "14.0");
-            androidCaps.setCapability("appium:skipUnlock", "true");
-            androidCaps.setCapability("appium:app", System.getProperty("user.dir") + "\\src\\main\\resources\\isinOlsun.apk");
-            androidCaps.setCapability("appium:fullReset", "true");
-            androidCaps.setCapability("appium:noReset", "false");
-            androidCaps.setCapability("appPackage", "com.isinolsun.app");
 
-            driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), androidCaps);
-            //wait = new WebDriverWait(driver, 10);
-            System.out.println("***** Android App *****");
+    private static AppiumDriver driver;
+
+
+    public static AppiumDriver getDriver() throws MalformedURLException {
+        DesiredCapabilities androidCaps = new DesiredCapabilities();
+        if(driver == null){
+            try {
+                driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), androidCaps);
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            }
         }
+        return driver;
+    }
+
+    public static void closeDriver(){
+        if(driver != null){
+            driver.quit();
+            driver = null;
+        }
+    }
+
 }
